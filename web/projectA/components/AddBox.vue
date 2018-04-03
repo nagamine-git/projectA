c076388a59ba3168f2366d8c535a33233ddcc187
nagaminenot <nagamine@slogan.jp>
2018-04-04 00:34:41 +0900

    タスク追加バー、表示だけ

---
+++ web/projectA/components/AddBox.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <v-layout row>
+    <v-flex xs4 sm4 md4>
+      <v-text-field
+      name="purpose"
+      label="目的"
+      id="purpose"
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs4 sm4 md4>
+      <v-text-field
+      name="todo"
+      label="やること"
+      id="purpose"
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs4 sm4 md4>
+      <v-text-field
+      name="condition"
+      label="条件"
+      id="condition"
+      ></v-text-field>
+    </v-flex>
+    <v-btn color="info">追加</v-btn>
+  </v-layout>
+
+</template>
+
+
+<style>
+
+</style>
--- web/projectA/layouts/default.vue
+++ web/projectA/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
+  <v-app>
+    <!-- <v-navigation-drawer
       :mini-variant="miniVariant"
       :clipped="clipped"
       v-model="drawer"
@@ -52,49 +52,34 @@
       >
         <v-icon>menu</v-icon>
       </v-btn>
-    </v-toolbar>
+    </v-toolbar> -->
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+    <!-- <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
-    }
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        { icon: "apps", title: "Welcome", to: "/" },
+        { icon: "bubble_chart", title: "Inspire", to: "/inspire" }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js"
+    };
   }
+};
 </script>
